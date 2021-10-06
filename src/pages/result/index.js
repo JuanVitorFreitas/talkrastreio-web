6a978c3ccde78a9cf7e04b155af55f907ae77539
Juan Freitas <juanvitor2001@outlook.com>
2021-10-06 18:29:46 -0300

    redirect if error returns code 500

--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,34 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import Header from '../../components/Header/Header';
 import api from '../../services/api';
 
 
 export default function Result() {
 
-	const [tracking, setTracking] = useState([]);
+	const [trackings, setTrackings] = useState([]);
+
+	let history = useHistory();
 
 	const { code } = useParams();
 
 	useEffect(() => {
 		async function fetchTracking() {
-			let response = await api.post('/', { trackingCode: code });
-			setTracking([response.data]);
+			try {
+				let response = await api.post('/', { trackingCode: code });
+				setTrackings([response.data]);
+			} catch (err) {
+				if (err.response.status === 500) {
+					history.push(`/`);
+					return alert('Erro ao buscar objeto, verifique o código e tente novamente.')
+				}
+			}
 		};
-
 		fetchTracking();
-	}, [code]);
+	}, [code, history]);
 
 
 	return (
 		<div>
+			<Header />
 			<ul>
-				{tracking.map((t) => (
-					<div>
-						<p>Código: {t.code}</p>
-						<p>Última atualização: {t.updatedAt}</p>
-					</div>
-				))}
-
+				{
+					trackings.map((t) => t.events.map((e) => (
+						<Card variant="outlined">
+							<CardContent>
+								<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+									<p>{e.date}</p>
+								</Typography>
+								<p>{e.unity}</p>
+								<p>{e.city}</p>
+								<p>{e.state}</p>
+								<p>{e.status}</p>
+								<p>{e.subStatus}</p>
+							</CardContent>
+						</Card>
+					)))
+				}
 			</ul>
 		</div>
 	)
