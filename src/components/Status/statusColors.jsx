390eeefb4a6202f5ec29a084975f057d7fbcda81
Juan Vitor Lobo de Freitas <juanvitor2001@outlook.com>
2021-10-12 22:49:02 -0300

    status colors and emojis

--- src/components/Status/statusColors.jsx
+++ src/components/Status/statusColors.jsx
@@ -3,8 +3,8 @@ const statusColors = {
 	'Objeto encaminhado': '#eccc68',
 	'Objeto postado após o horário limite da unidade': '#ffa502',
 	'Objeto recebido na unidade de exportação no país de origem': '#2ed573',
-	'Fiscalização Aduaneira finalizada': '#eccc68',
-	'Destinatário não retirou objeto no prazo': '#ad1d1d0',
+	'Fiscalização Aduaneira finalizada': '#2ed573',
+	'Destinatário não retirou objeto no prazo': '#ff3502',
 	'Objeto aguardando retirada no endereço indicado': '#ffa502',
 	'Objeto entregue ao destinatário': '#2ed573',
 	'Default': '#580463'
---
+++ src/components/Status/statusEmojis.jsx
@@ -0,0 +1,15 @@
+const statusEmojis = {
+	'Objeto saiu para entrega ao destinatário': '🤩',
+	'Objeto encaminhado': '🚚',
+	'Objeto postado após o horário limite da unidade': '🕦',
+	'Objeto recebido na unidade de exportação no país de origem': '⏱️🛫',
+	'Fiscalização aduaneira finalizada': '📦✅',
+	'Destinatário não retirou objeto no prazo': '📦❌',
+	'Objeto aguardando retirada no endereço indicado': '📩⏳',
+	'Objeto entregue ao destinatário': '🙌',
+	'Objeto recebido pelos Correios do Brasil': '📩🌎',
+	'Objeto postado': '📤',
+	'Default': ''
+}
+
+export default statusEmojis;
