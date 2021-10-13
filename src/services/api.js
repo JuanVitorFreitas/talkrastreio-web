2984983528e2c2e3e3e30703fbe79542951c9a73
Juan Vitor Lobo de Freitas <juanvitor2001@outlook.com>
2021-10-13 03:54:21 -0300

    fixed api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: process.env.API_URL || 'http://localhost:3333',
+	baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
 	timeout: 5000,
 });
