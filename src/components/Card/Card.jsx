4cc12da2ccd98cd6659d0ad5c7c04a240b622ef1
Juan Vitor Lobo de Freitas <juanvitor2001@outlook.com>
2021-10-12 22:51:50 -0300

    left border colored based on events

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,32 +1,25 @@
-import { Card as MaterialCard, CardContent, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
-import statusColors from '../../statusColors';
+import { Card as MaterialCard, CardContent, Skeleton, Typography } from '@material-ui/core';
+import statusColors from '../Status/statusColors';
+import statusEmojis from '../Status/statusEmojis';
 import { DateTime } from 'luxon';
-
-const useStyles = makeStyles({
-	card: {
-		backgroundColor: "#fff",
-		marginTop: '1rem',
-		borderRadius: '10px',
-		borderLeft: `5px solid ${statusColors.Default}`,
-	},
-	title: {
-		fontFamily: 'Courier New, monospace',
-	}
-});
 
 
 function Cards({ event }) {
 
-	const classes = useStyles();
-
 	return (
-		<MaterialCard className={classes.card} style={{ borderLeft: `5px solid ${statusColors[event.status]}` }} variant="outlined">
+		<MaterialCard sx={{
+			backgroundColor: "#fff",
+			marginTop: '1rem',
+			borderRadius: '10px',
+			borderLeft: `5px solid ${statusColors.Default}`,
+		}}
+			style={{ borderLeft: `5px solid ${statusColors[event.status]}` }}
+			variant="outlined">
 			<CardContent>
 				<Typography component="div">
 					<p>{event.unity} em {event.city} - {event.state}</p>
 				</Typography>
-				<p>{event.status} {DateTime.fromISO(event.date).toRelative({ locale: 'pt-br', style: 'narrow' },)}.</p>
+				<p>{event.status} {DateTime.fromISO(event.date).toRelative({ locale: 'pt-br', style: 'narrow' },)} {statusEmojis[event.status]}.</p>
 				<p dangerouslySetInnerHTML={{ __html: event.subStatus }}></p>
 			</CardContent>
 		</MaterialCard>
