f71dd5a8935c35a5eda0f404724890da3c2366c2
Juan Vitor Lobo de Freitas <juanvitor2001@outlook.com>
2021-10-13 03:37:24 -0300

    history component

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,38 +1,82 @@
-import { Card, CardContent, Container, Typography, List, ListItem, Button } from '@material-ui/core';
+import { CardContent, Container, Typography, List, ListItem, IconButton } from '@material-ui/core';
+import RedirectIcon from '@material-ui/icons/ExitToApp';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+import { useHistory } from 'react-router-dom';
+
 import useCodeHistory from '../../hooks/useCodeHistory';
-import ExitToApp from '@material-ui/icons/ExitToApp';
-import Delete from '@material-ui/icons/Delete';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-import Favorite from '@material-ui/icons/Favorite';
 
 function History() {
 
-	const { codeHistory } = useCodeHistory();
+	const history = useHistory();
+
+	const {
+		codeHistory,
+		removeFromHistory,
+		isCodeInFavorites,
+		toggleFavorite
+	} = useCodeHistory();
+
+	function clickRedirect(query) {
+		history.push(`/result/${query.code}`);
+	}
 
 	return (
 
 		<Container maxWidth="xs" >
-			<Card>
-				<CardContent>
-					<List style={{ textAlign: 'center' }}>
-						<Typography variant="h5" component="div">
-							Últimas Pesquisas
-						</Typography>
-						{codeHistory && codeHistory.map((c) => (
-							<ListItem sx={{
+			<CardContent sx={{ padding: '0' }}>
+				<List style={{ textAlign: 'center' }}>
+					<Typography variant="h5" sx={{ textAlign: 'center' }}>
+						Últimas Pesquisas
+					</Typography>
+					{codeHistory && codeHistory.map((query) => (
+						<ListItem
+							key={query.code}
+							sx={{
 								fontFamily: 'Noto Sans Mono, monospace',
+								padding: 0
 							}}>
-								{c.code}
-								<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-									<Button xs={{ padding: 0 }} variant="text"><ExitToApp /></Button>
-									<Button xs={{ padding: 0 }} variant="text"><FavoriteBorder /></Button>
-									<Button xs={{ padding: 0 }} variant="text"><Delete /></Button>
-								</div>
-							</ListItem>
-						))}
-					</List>
-				</CardContent>
-			</Card>
+							<Typography sx={{ textAlign: 'center', fontWeight: 500, fontFamily: 'Noto Sans Mono, monospace' }}>
+								{query.code}
+							</Typography>
+							<div
+								style={{
+									display: 'flex',
+									flexDirection: 'row',
+									alignItems: 'center',
+									marginLeft: 'auto',
+									paddingLeft: '0'
+								}}>
+								<IconButton
+									variant="text"
+									onClick={() => clickRedirect(query)}
+								>
+									<RedirectIcon
+										sx={{ color: '#580463' }}
+									/>
+								</IconButton>
+								<IconButton
+									variant="text"
+									onClick={() => toggleFavorite(query.code)}
+								>
+									{isCodeInFavorites(query.code) ? <FavoriteIcon sx={{ color: '#580463' }} /> : <FavoriteBorderIcon sx={{ color: '#580463' }} />}
+
+								</IconButton>
+								<IconButton
+									variant="text"
+									onClick={() => removeFromHistory(query.code)}
+								>
+									<DeleteIcon
+										sx={{ color: '#580463' }}
+									/>
+								</IconButton>
+							</div>
+						</ListItem>
+					))}
+				</List>
+			</CardContent>
 		</Container >
 	)
 }
--- src/contexts/historyContext.jsx
+++ src/contexts/historyContext.jsx
@@ -7,41 +7,76 @@ export default function HistoryContextProvider({ children }) {
 
 	// {code: string, timestamp: number}
 	const [codeHistory, setCodeHistory] = useState([]);
+	const [favorites, setFavorites] = useState([]);
 
 	useEffect(() => {
 		const storedHistory = JSON.parse(localStorage.getItem('@talkrastreio/codeHistory') || '[]');
-		console.log(`loaded history from storage`);
+		const storedFavorites = JSON.parse(localStorage.getItem('@talkrastreio/favorites') || '[]');
 		setCodeHistory(storedHistory);
+		setFavorites(storedFavorites);
 	}, []);
 
 	useEffect(() => {
 		if (codeHistory === null) {
 			return;
 		}
-		console.log(`saved history to storage`);
 		localStorage.setItem('@talkrastreio/codeHistory', JSON.stringify(codeHistory));
 	}, [codeHistory]);
 
-	function appendToHistory(code) {
-		console.log(`${code} added to history`);
 
-		// Previne qualquer elemento repetido no array
-		// if (codeHistory.some((c) => c.code === code)) {
-		//     return;
-		// }
 
-		// Previne dois elementos consecutivos iguais
-		if (codeHistory.length > 0 && codeHistory[codeHistory.length - 1].code === code) {
+	useEffect(() => {
+		if (favorites === null) {
 			return;
 		}
+		localStorage.setItem('@talkrastreio/favorites', JSON.stringify(favorites));
+	}, [favorites]);
 
-		setCodeHistory((current) => [...current, { code, timestamp: Date.now() }]);
+	function appendToHistory(code) {
+
+		// Previne dois elementos consecutivos iguais
+		// if (codeHistory.length > 0 && codeHistory[codeHistory.length - 1].code === code) {
+		// 	return;
+		// }
+
+		setCodeHistory((current) => [{ code, timestamp: Date.now() }, ...current.filter((c) => c.code !== code)]);
+	}
+
+	function appendToFavorites(code) {
+		const name = window.prompt("Insira o nome para este pacote", code);
+		setFavorites((current) => [...current, { code, name: name || code }]);
+	}
+
+	function removeFromFavorites(code) {
+		setFavorites((current) => current.filter((query) => code !== query.code));
+	}
+
+	function removeFromHistory(code) {
+		setCodeHistory((current) => current.filter((query) => code !== query.code));
+	}
+
+	function isCodeInFavorites(code) {
+		return favorites.some((f) => f.code === code)
+	}
+
+	function toggleFavorite(code) {
+		if (!isCodeInFavorites(code)) {
+			appendToFavorites(code);
+			return;
+		}
+		removeFromFavorites(code);
 	}
 
 	return (
 		<historyContext.Provider value={{
 			codeHistory,
-			appendToHistory
+			favorites,
+			appendToHistory,
+			removeFromHistory,
+			appendToFavorites,
+			removeFromFavorites,
+			toggleFavorite,
+			isCodeInFavorites
 		}}>
 			{children}
 		</historyContext.Provider>
